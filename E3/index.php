b1544ee3e915ebd22157f1e496d529ab0114b25b
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2021-11-15 20:57:39 -0500

    E3: retrieving files broken

--- E3/index.php
+++ E3/index.php
@@ -1,45 +1,51 @@
+<?php
+
+//check if there has been something posted to the server to be processed
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+
+// need to process
+ $color = $_POST['color'];
+ $size = $_POST['size'];
+ $speed = $_POST['speed'];
+ //run if there is a files array
+
+   //package the data and echo back
+    /* make  a new php object*/
+    $myPackagedData=new stdClass();
+    $myPackagedData->color = $color ;
+    $myPackagedData->size = $size ;
+    $myPackagedData->speed = $speed ;
+
+     /* Now we want to JSON encode these values as a JSON string ..
+     to send them to $.ajax success  call back function... */
+    $myJSONObj = json_encode($myPackagedData); //if it's not encoded in JSON, javascript won't be able to read it (doesnt read php)
+    // echo $myJSONObj;
+
+    $theFile = fopen("files/dataUserInput.txt", "a") or die("Unable to open file!");
+
+    fwrite($theFile, $myJSONObj);
+    fclose($theFile);
+
+    // echo $myJSONObj;
+    exit;
+}//POST
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Exercice 3</title>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+
  <link rel="stylesheet" href="css/exerciceStyle.css">
  <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
  <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
   </head>
-<?php
-//check if there has been something posted to the server to be processed
-if($_SERVER['REQUEST_METHOD'] == 'GET')
-{
-  //check if there is a new color (hence, new size and speed too); if so, append them to file;
-  if(isset($_GET['color'])){
-
-   $color = $_GET['color'];
-   $size = $_GET ['size'];
-   $speed = $_GET['speed'];
-  // echo($color);
-   //If you use fopen() on a file that does not exist, it will create it,
-   //given that the file is opened for writing (w) or appending (a).
-   $theFile = fopen("files/dataUserInput.txt", "a") or die("Unable to open file!");
-
-  fwrite($theFile, "COLOR:".$color."\n");
-  fwrite($theFile, "SIZE:".$size."\n");
-  // fwrite($theFile, $color); // no newline...
-  fwrite($theFile,  "SPEED:".$speed."\n");
-
-  fclose($theFile);
-//  echo("WE HAVE SUCCESSFULLY read the vars AND saved to the file ... ");
-   // you must exit
-exit;
-} //IF COLOR SET
-
-}//IF 'GET'
- ?>
-
 
   <body>
     <div class="container">
@@ -50,49 +56,51 @@ exit;
       <div id="preview">
 
       </div>
-      <form id="userInputForm">
-      <label for="color">Color</label><input type = "color" id="colorPicker" name="color" value="#e66465"required>
-      <label for="size">Size</label><input type="range" id="sizeSlider" name="size" min="10" max="100">
-      <label for="speed">Speed</label>  <input type="range" id="speedSlider" name="speed" min="1" max="20">
-      <input type = "submit" name = "submit" value = "send" id =buttonS />
+      <form id="userInputForm" action = "" enctype ="multipart/form-data">
+
+      <label for="color">General Mood</label><input type = "color" id="colorPicker" name="color" value="#8698b8"required>
+      <label for="size">Confidence Level</label><input type="range" id="sizeSlider" name="size" min="10" max="100">
+      <label for="speed">Anxiety Level</label>  <input type="range" id="speedSlider" name="speed" min="1" max="20">
+      <input type = "submit" name = "submit" value = "send" id = "buttonS" />
+
       </form>
     </div>
-  </body>
-
   <script>
   $(document).ready (function(){
       $("#userInputForm").submit(function(event) {
          //stop submit the form, we will post it manually. PREVENT THE DEFAULT behaviour ...
         event.preventDefault();
-       //console.log("button clicked");
-       let data =$('#userInputForm').serializeArray(); //serializeArray : puts form data and puts it into an array;
-       /*for console log */
-       for (let valuePairs of data.entries()) {
-        // console.log(valuePairs);
-       }
+    //   console.log("button clicked");
+       let form = $('#userInputForm')[0];
+     let dataForSending = new FormData(form);
+     /*console.log to inspect the data */
+    // for (let pair of dataForSending.entries()) {
+    //   console.log(pair[0]+ ', ' + pair[1]);
+    //   }
 
-       // P3
-           $.ajax({
-             type: "GET",
-              url: "index.php",
-              data: data,
-              dataType: "text", /*response will be text */
-              cache: false,
-              timeout: 600000,
-              success: function (response) {
-                //reponse is a STRING (not a JavaScript object -> so we need to convert)
-                //   console.log("we had success!");
-                //   console.log(response);
-                   //reset the form
-                   $('#userInputForm')[0].reset();
-                   //go to landing page
-                   window.location.replace("landing.php")
-             },
-             error:function(){
-            console.log("error occurred");
-          }
-        });
+     $.ajax({ //ajax is the most general function, we can be more specific with get, etc... Also very similar to other event handlers
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: "index.php",
+            data: dataForSending, //both can be called "data", this is just to make it clearer. 1st data : necessary parameter; 2nd data(ForSending): variable that comes from higher
+            processData: false,//prevents from converting into a query string
+            contentType: false, //contentType is the type of data you're sending,i.e.application/json; charset=utf-8
+            cache: false,
+            timeout: 600000,
+            success: function (response) {
+              //  console.log("we had success!");
+          //  console.log(response);
+
+            //go to landing page
+            window.location.replace("landing.php")
+           },
+           error:function(){
+           //This is where we end up if we have an error
+       console.log("error occurred");
+        }
+    });
     });
  });
    </script>
+  </body>
 </html>
--- E3/landing.php
+++ E3/landing.php
@@ -1,49 +1,127 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <?php
-  if($_SERVER['REQUEST_METHOD'] == 'GET')
-  {
-    // echo("here");
-    //get the data
-     //exit;
-     $theFile = fopen("files/dataUserInput.txt", "r") or die("Unable to open file!");
-     //read until eof
-     //$i=0;
-     $outArr = array();
-     $NUM_PROPS = 3;
-      //echo("test");
-        while(!feof($theFile)) {
-          //create an object to send back
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+  echo("here");
+  //get the data
+   //exit;
+   $theFile = fopen("files/dataUserInput.txt", "r") or die("Unable to open file!");
 
-          $packObj=new stdClass();
-
-          for($j=0;$j<$NUM_PROPS;$j++){
-            $str = fgets($theFile);
-            //split and return an array ...
-            $splitArr = explode(":",$str);
-            $key = $splitArr[0];
-            $val = $splitArr[1];
-            //append the key value pair
-            $packObj->$key = trim($val);
-          }
-          $outArr[]=$packObj;
+   //$i=0;
+   $outArr = array();
+   $NUM_PROPS = 3;
+    //echo("test");
+      while(!feof($theFile)) {   //read until eof
+        //create an object to send back
+        $packObj=new stdClass();
+        for($j=0;$j<$NUM_PROPS;$j++){
+          $str = fgets($theFile);
+          //split and return an array ...
+          $splitArr = explode(":",$str);
+          $key = $splitArr[0];
+          $val = $splitArr[1];
+          //append the key value pair
+          $packObj->$key = trim($val);
         }
+        $outArr[]=$packObj;
+      }
 
-        fclose($theFile);
-          // var_dump($outArr);
-          // Now we want to JSON encode these values to send them to $.ajax success.
-        $myJSONObj = json_encode($outArr);
-        echo $myJSONObj;
+      fclose($theFile);
+        // var_dump($outArr);
+        // Now we want to JSON encode these values to send them to $.ajax success.
+      $myJSONObj = json_encode($outArr);
+      echo $myJSONObj;
+} ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Community board</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <style>
+  body{
+    margin:0;
+    padding:0;
+  }
+  /* CAN ADD CSS  - > just like any other HTML element */
+  canvas{
+    background:black;
+    position:absolute;
+    top:0%;
+    left:0%;
+
+}
+  </style>
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Examples </title>
+  <style>
+  body{
+    margin:0;
+    padding:0;
+  }
+  canvas{
+    background:black;
+}
+  </style>
+
+  <script>
+
+  //userShape CLASS
+class userShape{
 
 
-        exit;
-  } ?>
-  <body>
+  constructor(x,y,color, size, speed){
 
-hello
-  </body>
+  }
+}
+
+  //ON LOAD
+  window.onload = function(){
+console.log(<?php echo$myJSONObj ?>);
+// get the canvas
+let canvas = document.getElementById("testCanvas");
+//Make canvas the size of the window
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight ;
+//Make canvas resize to the window
+window.addEventListener('resize', function(event) {
+//console.log("resize");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+});
+//get the context
+let context = canvas.getContext("2d");
+//would usually be black
+context.fillStyle = "#8ED6FF"; // change the color we are using
+// can use properties of the canvas object -> width, height ...
+//console.log(canvas);
+// note how we can use variables to generalize...
+let lineLength = 100;
+let x1 = canvas.width/2;
+let y1 =canvas.height/2;
+let x2 = x1+lineLength;
+let y2 = canvas.height/2;
+let x3 = x1+(lineLength/2);
+let y3 = y1-lineLength;
+
+// lets draw a triangle:
+//The lineTo() method adds a new point and creates a line
+//TO that point FROM the last specified point in the canvas
+//(this method does not draw the line) -rather the stroke/fill does.
+context.beginPath(); //start a path
+context.moveTo(x1,y1); //where to start drawing
+context.lineTo(x2,y2); //lineTo(where to go from last...)
+context.lineTo(x3,y3);
+context.lineTo(x1,y1);
+context.fill();
+context.strokeStyle = "#FFFFFF"; // change the color we are using
+context.lineWidth =2;
+context.stroke();
+context.closePath(); //end a path ...
+}//ONLOAD
+  </script>
+</head>
+<body>
+<canvas id = "testCanvas" width = 500 height =500></canvas>
+</body>
 </html>
