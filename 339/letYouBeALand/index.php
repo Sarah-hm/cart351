53ae1f15f5fa0e9f4baae7e86bbce28d584af20b
Sarah-hm <70521210+Sarah-hm@users.noreply.github.com>
2021-12-01 18:17:19 -0500

    339final : changed favicon

--- 339/letYouBeALand/index.php
+++ 339/letYouBeALand/index.php
@@ -65,6 +65,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['getAjaxOnLoad']))
 <html>
 <head>
   <title> Let you = a_land</title>
+  <link rel="shortcut icon" type="image/jpg" href="https://img.icons8.com/dotty/80/000000/globe-earth.png"/>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDTBA5GXAPNgRhVIvAxsgsuZpn2ezBGeCY&map_ids=4436878dd8c920c0"></script>
     <script>
